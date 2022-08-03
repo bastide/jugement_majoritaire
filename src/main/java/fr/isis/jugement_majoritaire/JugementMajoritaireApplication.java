34e35969731f6c00f6dbaef46a4a809ff5fb38a9
Rémi Bastide <Remi.Bastide@irit.fr>
2022-08-03 13:33:14 +0200

    first commit

---
+++ src/main/java/fr/isis/jugement_majoritaire/JugementMajoritaireApplication.java
@@ -0,0 +1,13 @@
+package fr.isis.jugement_majoritaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugementMajoritaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugementMajoritaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/isis/jugement_majoritaire/JugementMajoritaireApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isis.jugement_majoritaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JugementMajoritaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
